67210e56ede1687df8f99d56f7823a70ca687fab
ninozivan <zivan.nino@gmail.com>
2022-06-02 12:33:05 +0200

    Basic form fields for the configuration
    Added basic set of form fields for the configuration

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigSettingsFormComponent } from './components/config-settings-form/config-settings-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [ConfigSettingsFormComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IonicModule],
   exports: [ConfigSettingsFormComponent],
 })
 export class SharedModule {}
