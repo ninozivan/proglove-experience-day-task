01adbefb75ddb3ba21e02f7dc785f923918c95a3
ninozivan <zivan.nino@gmail.com>
2022-06-02 12:43:16 +0200

    Basic form - Form Group initialization
    Initialized basic angular form group

--- src/app/shared/components/config-settings-form/config-settings-form.component.ts
+++ src/app/shared/components/config-settings-form/config-settings-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/member-ordering */
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { IConfiguration } from 'src/assets/proto/configuration';
 
 @Component({
   selector: 'app-config-settings-form',
@@ -6,7 +14,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config-settings-form.component.scss'],
 })
 export class ConfigSettingsFormComponent implements OnInit {
-  constructor() {}
+  @Input() set itemConfig(config: IConfiguration) {
+    this.setupForm(config);
+  }
+
+  public formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {}
+
+  public setupForm(config: IConfiguration): void {
+    this.formGroup?.reset();
+
+    this.formGroup = this.formBuilder.group({
+      deviceName: new FormControl(config?.deviceName || null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(100),
+      ]),
+      timeout: new FormControl(config?.timeout || 0, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(10000),
+      ]),
+      audibleFeedback: new FormControl(config?.audibleFeedback || null),
+      visibleFeedback: new FormControl(config?.visibleFeedback || null),
+    });
+  }
 }
