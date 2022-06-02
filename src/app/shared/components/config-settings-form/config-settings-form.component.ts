1cc2b5158ed2a2017f5a14b9595e904e49f6e6cf
ninozivan <zivan.nino@gmail.com>
2022-06-02 15:03:10 +0200

    Refactoring of the form handling
    Implemented fully reactive config form initialization and update

--- src/app/shared/components/config-settings-form/config-settings-form.component.ts
+++ src/app/shared/components/config-settings-form/config-settings-form.component.ts
@@ -1,13 +1,21 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable @typescript-eslint/member-ordering */
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnDestroy,
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { auditTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { auditTime, takeUntil } from 'rxjs/operators';
 
 import { Configuration, IConfiguration } from 'src/assets/proto/configuration';
 
@@ -17,7 +25,7 @@ enum FeedbackTypes {
 }
 
 interface IFeedbackType {
-  enum: FeedbackTypes;
+  type: FeedbackTypes;
   title: string;
 }
 
@@ -26,35 +34,50 @@ interface IFeedbackTypeOption {
   title: string;
 }
 
+interface IFormValue {
+  deviceName: string;
+  timeout: number;
+  feedbackType: FeedbackTypes;
+  feedbackOption: IFeedbackTypeOption;
+}
+
 @Component({
   selector: 'app-config-settings-form',
   templateUrl: './config-settings-form.component.html',
   styleUrls: ['./config-settings-form.component.scss'],
 })
-export class ConfigSettingsFormComponent implements OnInit {
+export class ConfigSettingsFormComponent implements OnInit, OnDestroy {
   @Output() formValueChanged: EventEmitter<IConfiguration> =
     new EventEmitter<IConfiguration>();
 
   @Input() set itemConfig(config: IConfiguration) {
-    this.setupForm(config);
+    const tmpConfig: IConfiguration = {
+      timeout: 700,
+      deviceName: 'Device 1',
+      audibleFeedback: null,
+      visibleFeedback: Configuration.VisibleFeedback.GREEN,
+    };
+    this.setupForm(tmpConfig);
   }
 
+  public destroyed$: Subject<boolean> = new Subject();
   public formGroup!: FormGroup;
 
   public initialFeedbackTypeValue: FeedbackTypes =
     FeedbackTypes.VisibleFeedback;
+
   public feedbackTypes: IFeedbackType[] = [
     {
-      enum: FeedbackTypes.AudibleFeedback,
+      type: FeedbackTypes.AudibleFeedback,
       title: 'Audible',
     },
     {
-      enum: FeedbackTypes.VisibleFeedback,
+      type: FeedbackTypes.VisibleFeedback,
       title: 'Visible',
     },
   ];
 
-  public audibleFeedbackTypes: IFeedbackTypeOption[] = [
+  public audibleOptions: IFeedbackTypeOption[] = [
     {
       enum: Configuration.AudibleFeedback.BEEP,
       title: 'BEEP',
@@ -73,7 +96,7 @@ export class ConfigSettingsFormComponent implements OnInit {
     },
   ];
 
-  public visibleFeedbackTypes: IFeedbackTypeOption[] = [
+  public visibleOptions: IFeedbackTypeOption[] = [
     {
       enum: Configuration.VisibleFeedback.GREEN,
       title: 'GREEN',
@@ -88,19 +111,25 @@ export class ConfigSettingsFormComponent implements OnInit {
     },
   ];
 
-  public selectedFeedbackOption!: IFeedbackTypeOption;
   public feedbackTypeOptions!: IFeedbackTypeOption[];
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {}
 
-  public get selectedFeedbackOptionValue(): number {
-    return this.selectedFeedbackOption?.enum;
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 
   public setupForm(config: IConfiguration): void {
     this.formGroup?.reset();
+
+    const initialFeedbackType: IFeedbackType =
+      this.provideInitialFeedbackTypeOption(config);
+    const initialFeedbackTypeOption: IFeedbackTypeOption =
+      this.provideInitialOptionForFeedbackType(config);
+    this.feedbackTypeOptions = this.provideInitialOptionsForFeedback(config);
 
     this.formGroup = this.formBuilder.group({
       deviceName: new FormControl(config?.deviceName || null, [
@@ -113,40 +142,90 @@ export class ConfigSettingsFormComponent implements OnInit {
         Validators.min(1),
         Validators.max(10000),
       ]),
-      audibleFeedback: new FormControl(config?.audibleFeedback || null),
-      visibleFeedback: new FormControl(config?.visibleFeedback || null),
+      feedbackType: new FormControl(initialFeedbackType),
+      feedbackOption: new FormControl(initialFeedbackTypeOption),
     });
 
     this.formGroup?.valueChanges
-      ?.pipe(auditTime(300))
-      .subscribe((formValues: IConfiguration) => {
-        this.formValueChanged?.emit(formValues);
+      ?.pipe(takeUntil(this.destroyed$), auditTime(300))
+      .subscribe((formValues: IFormValue) => {
+        const newConfig: IConfiguration = {
+          deviceName: formValues?.deviceName,
+          timeout: formValues?.timeout,
+          visibleFeedback:
+            formValues?.feedbackType === FeedbackTypes.AudibleFeedback
+              ? formValues.feedbackOption.enum
+              : null,
+          audibleFeedback:
+            formValues?.feedbackType === FeedbackTypes.VisibleFeedback
+              ? formValues.feedbackOption.enum
+              : null,
+        };
+
+        this.formValueChanged?.emit(newConfig);
       });
 
-    this.handleFeedbackTypeChange(FeedbackTypes.VisibleFeedback);
+    this.formGroup?.controls?.feedbackType?.valueChanges
+      ?.pipe(takeUntil(this.destroyed$), auditTime(300))
+      .subscribe((value: FeedbackTypes) => {
+        this.feedbackTypeOptions =
+          value === FeedbackTypes.AudibleFeedback
+            ? this.audibleOptions
+            : this.visibleOptions;
+
+        const newOptionForFeedback: IFeedbackTypeOption =
+          value === FeedbackTypes.AudibleFeedback
+            ? this.audibleOptions[0]
+            : this.visibleOptions[0];
+        this.formGroup?.controls?.feedbackOption?.setValue(
+          newOptionForFeedback
+        );
+
+        this.formGroup?.updateValueAndValidity();
+      });
   }
 
-  public onFeedbackTypeChanged(event: Event): void {
-    const selectedFeedbackType: any = (event as CustomEvent)?.detail?.value;
-    this.handleFeedbackTypeChange(selectedFeedbackType);
-  }
-
-  public onFeedbackTypeOptionChanged(event: Event): void {
-    const selectedFeedbackTypeOption: any = (event as CustomEvent)?.detail
-      ?.value;
-  }
-
-  private handleFeedbackTypeChange(selectedType: FeedbackTypes): void {
-    if (selectedType === FeedbackTypes.AudibleFeedback) {
-      this.formGroup?.controls?.visibleFeedback?.setValue(null);
-      this.feedbackTypeOptions = this.audibleFeedbackTypes;
-      this.selectedFeedbackOption = this.audibleFeedbackTypes[0];
-    } else if (selectedType === FeedbackTypes.VisibleFeedback) {
-      this.formGroup?.controls?.visibleFeedback?.setValue(null);
-      this.feedbackTypeOptions = this.visibleFeedbackTypes;
-      this.selectedFeedbackOption = this.visibleFeedbackTypes[0];
+  private provideInitialFeedbackTypeOption(
+    config: IConfiguration
+  ): IFeedbackType | null {
+    if (typeof config?.audibleFeedback === 'number') {
+      return this.feedbackTypes?.find(
+        (item: IFeedbackType) => item?.type === FeedbackTypes.AudibleFeedback
+      );
+    } else if (typeof config?.visibleFeedback === 'number') {
+      return this.feedbackTypes?.find(
+        (item: IFeedbackType) => item?.type === FeedbackTypes.VisibleFeedback
+      );
     }
 
-    this.formGroup?.updateValueAndValidity();
+    return null;
+  }
+
+  private provideInitialOptionForFeedbackType(
+    config: IConfiguration
+  ): IFeedbackTypeOption | null {
+    if (typeof config?.audibleFeedback === 'number') {
+      return this.audibleOptions?.find(
+        (item: IFeedbackTypeOption) => item?.enum === config?.audibleFeedback
+      );
+    } else if (typeof config?.visibleFeedback === 'number') {
+      return this.visibleOptions?.find(
+        (item: IFeedbackTypeOption) => item?.enum === config?.visibleFeedback
+      );
+    }
+
+    return null;
+  }
+
+  private provideInitialOptionsForFeedback(
+    config: IConfiguration
+  ): IFeedbackTypeOption[] {
+    if (typeof config?.audibleFeedback === 'number') {
+      return this.audibleOptions;
+    } else if (typeof config?.visibleFeedback === 'number') {
+      return this.visibleOptions;
+    }
+
+    return [];
   }
 }
