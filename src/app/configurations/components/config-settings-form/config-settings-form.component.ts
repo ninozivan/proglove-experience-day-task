487365b053d15c50336c07800fb77b7b2fb48ddb
ninozivan <zivan.nino@gmail.com>
2022-06-02 11:57:59 +0200

    New component - Config Settings form
    Created new component that will be used to hold form fields that will be available to the user to edit for each configuration

---
+++ src/app/configurations/components/config-settings-form/config-settings-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-config-settings-form',
+  templateUrl: './config-settings-form.component.html',
+  styleUrls: ['./config-settings-form.component.scss'],
+})
+export class ConfigSettingsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/configurations/configurations.module.ts
+++ src/app/configurations/configurations.module.ts
@@ -7,14 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ConfigurationsPageRoutingModule } from './configurations-routing.module';
 
 import { ConfigurationsPage } from './configurations.page';
+import { ConfigSettingsFormComponent } from './components/config-settings-form/config-settings-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ConfigurationsPageRoutingModule
+    ConfigurationsPageRoutingModule,
   ],
-  declarations: [ConfigurationsPage]
+  declarations: [ConfigurationsPage, ConfigSettingsFormComponent],
 })
 export class ConfigurationsPageModule {}
